6497f41667ebead9094a876f3b3c6500282a5302
DK <jack.chang@trentglobal.edu.sg>
2022-08-10 11:00:31 +0000

    added search

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const { ObjectId } = require('mongodb');
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
 app.use(cors());
 
 const MONGO_URI = process.env.MONGO_URI;
@@ -15,16 +15,57 @@ async function main() {
 
     const db = await connect(MONGO_URI, DB_NAME);
 
-    app.post('/add', async function(req,res){
-        let result = await db.collection('').insertOne({
-            
+    app.post('/add', async function (req, res) {
+        let result = await db.collection('earphone').insertOne({
+            'brand': req.body.brand,
+            'type': req.body.type,
+            'bluetooth': req.body.bluetooth,
+            'price': req.body.price,
+            'color': req.body.color,
+            'time': req.body.time,
+            'dustproof-and-waterproof': req.body.proof,
+            'connectors': req.body.connectors
         })
     })
+    
+    app.get('/recipes',async function(req,res){
+        let criteria = {};
+        if(req.query.type) {
+            criteria.type = {
+                '$regex': req.query.type, '$options': 'i'
+            }
+        }
 
+        if(req.query.connectors) {
+            criteria.connectors = {
+                '$regex': req.query.connectors, '$options': 'i'
+            }
+        }
 
+        if(req.query.min_price) {
+            criteria.price = {
+                '$gte': parseInt(req.query.min_price)
+            }
+        }
+
+        if(req.query.max_price) {
+            criteria.price = {
+                '$lte': parseInt(req.query.max_price)
+            }
+        }
+
+        let result = await db.collection('bluetooth').find(criteria, {
+            'projection': {
+                'brand': 1,
+                'color': 1
+            }
+        }).toArray();
+        res.status(200);
+        res.send(result);
+    })
 
 }
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log('server started!!!')
 })
