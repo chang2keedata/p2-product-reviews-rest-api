b79e9d9076be7af2ef94f9934429b2284255670d
DK <jack.chang@trentglobal.edu.sg>
2022-08-22 13:33:13 +0000

    json sample and res msg updated

--- index.js
+++ index.js
@@ -15,17 +15,23 @@ async function main() {
 
     const db = await connect(MONGO_URI, DB_NAME);
 
-    app.post('/add', async function (req, res) {
+    app.post('/add', async function (req,res) {
         let result = await db.collection('earphone').insertOne({
             'brand': req.body.brand,
+            'model': req.body.model,
             'type': req.body.type,
-            'bluetooth': req.body.bluetooth,
-            'price': req.body.price,
+            'earbuds': req.body.earbuds,
+            'bluetooth': parseInt(req.body.bluetooth),
+            'price': parseInt(req.body.price),
+            "sale": req.body.sale,
             'color': req.body.color,
             'time': req.body.time,
-            'dustproof-and-waterproof': req.body.proof,
-            'connectors': req.body.connectors
+            'dustWaterproof': req.body.dustWaterproof,
+            'connectors': req.body.connectors,
         })
+        res.status(200).json({
+            status: 200
+        });
     })
     
     app.get('/earphone',async function(req,res){
@@ -57,11 +63,17 @@ async function main() {
         let result = await db.collection('earphone').find(criteria, {
             'projection': {
                 'brand': 1,
-                'color': 1
+                'model': 1,
+                'type': 1,
+                'bluetooth': 1,
+                'price': 1,
+                'color': 1,
+                'time': 1,
+                'dustWaterproof': 1,
+                'connectors': 1
             }
         }).toArray();
-        res.status(200);
-        res.send(result);
+        res.status(200).send(result);
     })
 
     app.put('/earphone/:id',async function(req,res){
@@ -74,20 +86,30 @@ async function main() {
         },{
             '$set': {
                 'brand': req.body.brand ? req.body.brand : earphone.brand,
-                'type': req.body.type ? req.body.type : earphone.description,
-                'price': req.body.price ? req.body.price : earphone.price
+                'model': req.body.model ? req.body.model : earphone.model,
+                'type': req.body.type ? req.body.type : earphone.type,
+                'earbuds': req.body.earbuds ? req.body.earbuds : earphone.earbuds,
+                'bluetooth': req.body.bluetooth ? req.body.bluetooth : earphone.bluetooth,
+                'price': req.body.price ? req.body.price : earphone.price,
+                'sale': req.body.sale ? req.body.sale : earphone.sale,
+                'color': req.body.color ? req.body.color : earphone.color,
+                'time': req.body.time ? req.body.time : earphone.time,
+                'dustWaterproof': req.body.dustWaterproof ? req.body.dustWaterproof : earphone.dustWaterproof,
+                'connectors': req.body.connectors ? req.body.connectors : earphone.connectors
             }
         })
-        res.status(200);
-        res.send(result);
+        res.status(200).json({
+            'message': 'Updated succesfully'
+        });
     })
 
     app.delete('/earphone/:id',async function(req,res){
         await db.collection('earphone').deleteOne({
             '_id': ObjectId(req.params.id)
         })
-        res.status(200);
-        res.send(result);
+        res.status(200).json({
+            'message': 'deleted successfully'
+        });
     })
 
     app.post('/earphone/:id/reviews',async function(req,res){
@@ -139,7 +161,7 @@ async function main() {
             '$set': {
                 'review.$.email': req.body.email ? req.body.email : review.email,
                 'review.$.content': req.body.content ? req.body.content : review.content,
-                'review.$.date': req.body.date ? new Date(req.body.date) : new Date();
+                'review.$.date': req.body.date ? new Date(req.body.date) : new Date()
             }
         })
         res.status(200);
@@ -159,7 +181,10 @@ async function main() {
         res.status(200);
         res.send(result);
     })
+
+
 }
+main();
 
 app.listen(3000, function () {
     console.log('server started!!!')
