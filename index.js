e80f343d89513e1c1ec40e0a880f1d117ac40ad8
DK <jack.chang@trentglobal.edu.sg>
2022-08-24 10:35:24 +0000

    added user sign in

--- index.js
+++ index.js
@@ -11,13 +11,31 @@ app.use(cors());
 const MONGO_URI = process.env.MONGO_URI;
 const DB_NAME = process.env.DB_NAME;
 
+function checkIfAuthenticationJWT(req,res,next) {
+    if(req.headers.authorization) {
+        const headers = req.headers.authorization;
+        const token = headers.split(' ')[1];
+
+        jwt.verify(token, process.env.TOKEN_SECRET, function(err, tokenData){
+            if(err) {
+                res.sendStatus(403);
+                return;
+            }
+            req.student = tokenData;
+            next();
+        })
+    } else {
+        res.sendStatus(403);
+    }
+}
+
 async function main() {
 
     const db = await connect(MONGO_URI, DB_NAME);
 
     // ADD NEW PRODUCT
     app.post('/add', async function (req,res) {
-        let result = await db.collection('earphone').insertOne({
+        await db.collection('earphone').insertOne({
             'brand': req.body.brand,
             'model': req.body.model,
             'type': req.body.type,
@@ -102,7 +120,7 @@ async function main() {
             '_id': ObjectId(req.params.id)
         })
 
-        let result = await db.collection('earphone').updateOne({
+        await db.collection('earphone').updateOne({
             '_id': ObjectId(req.params.id)
         },{
             '$set': {
@@ -134,8 +152,8 @@ async function main() {
         });
     })
 
-    // REVIEW PRODUCT
-    app.post('/earphone/:id/reviews',async function(req,res){
+    // ADD PRODUCT REVIEW 
+    app.post('/earphone/:id/review',async function(req,res){
         let result = await db.collection('earphone').updateOne({
             '_id': ObjectId(req.params.id)
         },{
@@ -145,7 +163,7 @@ async function main() {
                     'email': req.body.email,
                     'comment': req.body.content,
                     'rating': req.body.rating,
-                    'date': req.body.date
+                    'date': new Date()
                 }
             }
         })
@@ -153,14 +171,15 @@ async function main() {
         res.send(result);
     })
 
-    app.get('/earphone/:id/reviews',async function(req,res){
+    // GET A REVIEW
+    app.get('/earphone/:id/review',async function(req,res){
         let result = await db.collection('earphone').findOne({
             '_id': ObjectId(req.params.id)
         },{
             'projection': {
                 '_id': 1,
                 'brand': 1,
-                'type': 1,
+                'model': 1,
                 'review': 1
             }
         })
@@ -168,7 +187,8 @@ async function main() {
         res.send(result);
     })
 
-    app.put('/earphone/:id/reviews/:reviewid',async function(req,res){
+    // EDIT THE REVIEW
+    app.put('/earphone/:id/review/:reviewid',async function(req,res){
         let review = await db.collection('earphone').findOne({
             '_id': ObjectId(req.params.id),
             'review._id': ObjectId(req.params.reviewid)
@@ -184,7 +204,8 @@ async function main() {
         },{
             '$set': {
                 'review.$.email': req.body.email ? req.body.email : review.email,
-                'review.$.content': req.body.content ? req.body.content : review.content,
+                'review.$.comments': req.body.comments ? req.body.comments : review.comments,
+                'review.$.rating': req.body.rating ? req.body.rating : review.rating,
                 'review.$.date': req.body.date ? new Date(req.body.date) : new Date()
             }
         })
@@ -192,7 +213,8 @@ async function main() {
         res.send(result);
     })
 
-    app.delete('/earphone/:id/reviews/:reviewid',async function(req,res){
+    // DELETE REVIEW
+    app.delete('/earphone/:id/review/:reviewid',async function(req,res){
         let result = await db.collection('earphone').deleteOne({
             '_id': ObjectId(req.params.id)
         },{
@@ -206,6 +228,57 @@ async function main() {
         res.send(result);
     })
 
+    // SIGNUP - ADD NEW USER
+    app.post('/user',async function(req,res){
+        await db.collection('users').insertOne({
+            'username': req.body.username,
+            'firstname': req.body.firstname,
+            'lastname': req.body.lastname,
+            'email': req.body.email,
+            'password': req.body.password,
+        })
+        res.sendStatus(201);
+    })
+
+    // LOGIN
+    app.post('/login',async function(req,res){
+        let users = await db.collection('users').findOne({
+            'email': req.body.email,
+            'password': req.body.password
+        })
+        if(users) {
+            let token = jwt.sign({
+                'username': req.body.username,
+                'firstname': req.body.firstname,
+                'lastname': req.body.lastname
+            },process.env.TOKEN_SECRET,{
+                'expiresIn': '1h'
+            })
+            res.json({
+                'accessToken': token
+            })
+        } else {
+            res.sendStatus(401)
+        }
+    })
+
+    // UPDATE PROFILE
+    app.put('/user/:id',[checkIfAuthenticationJWT],async function(req,res){
+        let student = await db.collection('users').findOne({
+            '_id': ObjectId(req.params.id)
+        })
+
+        let result = await db.collection('users').updateOne({
+            '_id': ObjectId(req.params.id)
+        },{
+            '$set': {
+                'username': req.body.username ? req.body.username : student.username,
+                'firstname': req.body.firstname ? req.body.firstname : student.firstname,
+                'firstname': req.body.firstname ? req.body.firstname : student.firstname
+            }
+        })
+        res.sendStatus(200);
+    })
 
 }
 main();
